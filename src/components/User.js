c3e05df20b0d02821563cf5ecea076edf619cb49
mayrant <mayrant2004@yahoo.com>
2021-10-04 17:15:43 -0500

    all components functional

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function User(props) {
-  const { email, picture: { thumbnail }, name: { first, last } } = props.person;
-  
+  const { 
+    email, 
+    gender,
+    cell, 
+    picture: { thumbnail }, 
+    name: { first, last },  
+    dob: { age }, 
+    location: { state, country }
+  } = props.person;
+
+  const [isHidden, setHidden] = useState(true);
+
+  const handleDetails = () => {
+    setHidden(!isHidden)
+    console.log('clicked')
+  }
+
   return (
     <li key={email}>
       <img src={thumbnail} alt={first} />
       <h3>{first} {last}</h3>
-      <button>Show Details</button>
+      <button onClick={() => handleDetails()}>{isHidden ? "Show Details" : "Hide Details"}</button>
+      {!isHidden && 
+        <div className="user-details">
+          {gender}, {age}<br/>
+          {state}, {country}<br/>
+          email: {email}<br/>
+          cell: {cell}
+        </div>
+      }
     </li>
   )
 }
